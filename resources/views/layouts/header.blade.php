a9f235dd808a3d794645439ce82f6927b6f12c8c
Vladimir <dadyka.vova@gmail.com>
2022-02-20 10:42:27 +0300

    fixed deprecated bootstrap

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,9 +1,9 @@
 @section('header')
-    <div class="margin1200">
+    <div>
         {{-- Вывод сообщений об ошибках и об успехе --}}
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1"
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-target="#navbar1"
                     aria-controls="navbar1" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -34,7 +34,7 @@
                     {{--<!--                    --><?php //if (in_array($_SESSION['priority'], [2, 7])) { ?>--}}
                     @can('canLoginToIdsMsk')
                         <li class="nav-item my-auto">
-                            <a class="nav-link" href="https://moskva.03-okna.ru/total_order.php">IDS&nbsp;msk</a>
+                            <a class="btn nav-link" href="https://moskva.03-okna.ru/total_order.php">IDS&nbsp;msk</a>
                         </li>
                     @endcan
 
@@ -52,174 +52,215 @@
                     @can('addOrders')
                         {{--<!--                    --><?php //if (in_array($_SESSION['priority'], [0, 1, 2, 4, 6, 7])) { ?>--}}
                         <li class="nav-item dropdown my-auto">
-                            <a class="nav-link dropdown-toggle"
-                               type="button"
-                               id="navbarDropdown1"
-                               data-toggle="dropdown"
+                            <a class="btn nav-link dropdown-toggle"
+                               href="#"
+                               role="button"
+                               id="navBarOrders"
+                               data-bs-toggle="dropdown"
                                aria-haspopup="true"
-                               aria-expanded="false">Заказы</a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
-                                <a class="dropdown-item"
-                                    href="/add_order.php">Добавить
-                                    заказ</a>
-                                <a class="dropdown-item" href="/load.php?route=admin/order/all">Предыдущие заказы</a>
-                                <a class="dropdown-item" href="/load.php?route=admin/order/calc">Предыдущие расчеты</a>
-                                <a
-                                    class="dropdown-item"
-                                    href="/load.php?route=admin/plan/steklopaket">Заказы
-                                    по стеклопакетам</a>
-                                <a
-                                    class="dropdown-item"
-                                    href="/load.php?route=admin/plan/podokonnik">Заказы
-                                    по подоконникам</a>
-                                <a
-                                    class="dropdown-item"
-                                    href="/load.php?route=admin/stat/control">Контроль
-                                    качества на заказах</a>
-                            </div>
+                               aria-expanded="false">Заказы
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navBarOrders">
+                                <li>
+                                    <a class="dropdown-item"
+                                       href="/add_order.php">Добавить
+                                        заказ
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="/load.php?route=admin/order/all">Предыдущие заказы
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="/load.php?route=admin/order/calc">Предыдущие
+                                        расчеты
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        class="dropdown-item"
+                                        href="/load.php?route=admin/plan/steklopaket">Заказы
+                                        по стеклопакетам
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        class="dropdown-item"
+                                        href="/load.php?route=admin/plan/podokonnik">Заказы
+                                        по подоконникам
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        class="dropdown-item"
+                                        href="/load.php?route=admin/stat/control">Контроль
+                                        качества на заказах
+                                    </a>
+                                </li>
+                            </ul>
                         </li>
                     @endcan
 
                     @can('seeManagement')
                         <li class="nav-item dropdown my-auto"> {{-- nav-item dropdown my-auto --}}
-                            <a class="nav-link dropdown-toggle" id="navbarDropdown1" role="button"
-                               data-toggle="dropdown"
-                               aria-haspopup="true" aria-expanded="false">Управление</a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
+                            <a href='#' class="btn nav-link dropdown-toggle" id="navBarManagement" role="button"
+                               data-bs-toggle="dropdown"
+                               aria-haspopup="true" aria-expanded="false">Управление
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navBarManagement">
                                 <a class="dropdown-item" href="/load.php?route=admin/price/index">Цены на товары и
-                                    услуги</a>
+                                    услуги
+                                </a>
 
                                 <div class="dropdown-divider"></div>
                                 <a class="dropdown-item" href="/load.php?route=admin/phone/zadarma">Звонки</a>
                                 <div class="dropdown-divider"></div>
 
                                 <a class="dropdown-item" href="/load.php?route=admin/sborshik/graph">График
-                                    работы сотрудников</a>
+                                    работы сотрудников
+                                </a>
 
                                 <a class="dropdown-item"
-                                   href="/load.php?route=admin/stat/index&type=stat">Статистика</a>
+                                   href="/load.php?route=admin/stat/index&type=stat">Статистика
+                                </a>
                                 <a class="dropdown-item" href="/load.php?route=admin/stat/index&type=plan">План + стат.
-                                    менеджеров</a>
+                                    менеджеров
+                                </a>
                                 <a class="dropdown-item"
-                                   href="/load.php?route=admin/stat/index&type=proisvodstvo">Производство</a>
+                                   href="/load.php?route=admin/stat/index&type=proisvodstvo">Производство
+                                </a>
                                 <a class="dropdown-item" href="/load.php?route=admin/stat/index&type=phone">Настройка
-                                    звонков</a>
+                                    звонков
+                                </a>
                                 <a class="dropdown-item"
-                                   href="/load.php?route=admin/stat/index&type=user">Пользователи</a>
+                                   href="/load.php?route=admin/stat/index&type=user">Пользователи
+                                </a>
                                 <a class="dropdown-item" href="/load.php?route=admin/stat/index&type=money">Деньги</a>
                                 <a class="dropdown-item"
-                                   href="/load.php?route=admin/stat/index&type=other">Прочее</a>
+                                   href="/load.php?route=admin/stat/index&type=other">Прочее
+                                </a>
                             </div>
                         </li>
                     @endcan
                     @can('seeDocuments')
                         <li class="nav-item my-auto">
-                            <a class="nav-link" href="#">Документы</a>
+                            <a class="btn nav-link" href="#">Документы</a>
                         </li>
                     @endcan
 
                     @can('seePlan')
                         <li class="nav-item my-auto">
-                            <a class="nav-link" href="/load.php?route=admin/plan/my">План<span
+                            <a class="btn nav-link" href="/load.php?route=admin/plan/my">План<span
                                     id="plan_not_readed_hd"></span></a>
                         </li>
                     @endcan
                     @can('seeSpecs')
                         <li class="nav-item my-auto">
-                            <a class="nav-link" href="#">Спецификации</a>
+                            <a class="btn nav-link" href="#">Спецификации</a>
                         </li>
                     @endcan
 
                     @can('seeTodaysGraph')
                         <li class="nav-item my-auto">
-                            <a class="nav-link" href="/load.php?route=admin/graph/deliv&date=<?= date('d.m.Y'); ?>">График
+                            <a class="btn nav-link" href="/load.php?route=admin/graph/deliv&date=<?= date('d.m.Y'); ?>">
+                                График
                                 на
-                                сегодня</a>
+                                сегодня
+                            </a>
                         </li>
                     @endcan
                     @can('seeExtendedPlan')
                         <li class="nav-item dropdown my-auto">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">План
+                            <a class="btn nav-link dropdown-toggle" href="#" id="navBarPlan" role="button"
+                               data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">План
                             </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
+                            <div class="dropdown-menu" aria-labelledby="navBarPlan">
                                 <a class="dropdown-item" href="/load.php?route=admin/plan/my">Мой план</a>
                                 <a class="dropdown-item" href="/load.php?route=admin/plan/all">План всех
-                                    сотрудников</a>
+                                    сотрудников
+                                </a>
                                 <a class="dropdown-item" href="/load.php?route=admin/plan/dolgo">Долгосрочный план
-                                    всех</a>
+                                    всех
+                                </a>
                                 <a class="dropdown-item" href="/load.php?route=admin/plan/add_plan">Добавить и ред.
-                                    задания</a>
+                                    задания
+                                </a>
                             </div>
                         </li>
                     @endcan
                     @can('seeNotificationsForInstallers')
                         <li class="nav-item dropdown my-auto">
-                            <a class="nav-link" href="/news.php">Увед. для монт.</a>
+                            <a class="btn nav-link" href="/news.php">Увед. для монт.</a>
                         </li>
                     @endcan
                     @can('seeWages')
                         <li class="nav-item dropdown my-auto">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button"
-                               data-toggle="dropdown"
-                               aria-haspopup="true" aria-expanded="false">Зарплаты</a>
+                            <a class="btn nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button"
+                               data-bs-toggle="dropdown"
+                               aria-haspopup="true" aria-expanded="false">Зарплаты
+                            </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
                                 <a class="dropdown-item" href="/load.php?route=admin/master/zp">Зарплаты
-                                    монтажникам</a>
+                                    монтажникам
+                                </a>
                                 <a class="dropdown-item" href="/load.php?route=admin/sborshik/graph">Зарплаты
-                                    менеджерам</a>
+                                    менеджерам
+                                </a>
                                 <a class="dropdown-item" href="/load.php?route=admin/master/history">Бонусы
-                                    для монтажников</a>
+                                    для монтажников
+                                </a>
                             </div>
                         </li>
                     @endcan
 
                     @can('canCalculate')
                         <li class="nav-item dropdown my-auto">
-                            <a class="nav-link" href="#">Рассчитать</a>
+                            <a class="btn nav-link" href="#">Рассчитать</a>
                         </li>
                     @endcan
 
                     @can('seeHisGraph')
                         <li class="nav-item dropdown my-auto">
-                            <a class="nav-link" href="/load.php?route=admin/graph/mont">График</a>
+                            <a class="btn nav-link" href="/load.php?route=admin/graph/mont">График</a>
                         </li>
                     @endcan
 
                     @can('seeGraphs')
                         <li class="nav-item dropdown my-auto">
-                            <a class="nav-link" href="/load.php?route=admin/graph/all">Графики</a>
+                            <a class="btn nav-link" href="/load.php?route=admin/graph/all">Графики</a>
                         </li>
                     @endcan
 
                     @can('seeMap')
                         <li class="nav-item dropdown my-auto">
-                            <a class="nav-link" href="/maps.php">Карта</a>
+                            <a class="btn nav-link" href="/maps.php">Карта</a>
                         </li>
                     @endcan
 
                     @can('seeReceipts')
                         <li class="nav-item dropdown my-auto">
-                            <a class="nav-link" href="/load.php?route=admin/prihod/prihod">Поступления</a>
+                            <a class="btn nav-link" href="/load.php?route=admin/prihod/prihod">Поступления</a>
                         </li>
                     @endcan
 
                     @can('seeWarehouse')
                         <li class="nav-item dropdown my-auto">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Склад</a>
+                            <a class="btn nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button"
+                               data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Склад
+                            </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
                                 <a class="dropdown-item" href="#">Остатки на складе</a>
                                 <a class="dropdown-item"
                                    href="#">История
-                                    перемещений</a>
+                                    перемещений
+                                </a>
                             </div>
                         </li>
                     @endcan
 
                     {{--                    <li class="nav-item dropdown my-auto">--}}
                     {{--                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button"--}}
-                    {{--                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Информация</a>--}}
+                    {{--                           data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Информация</a>--}}
                     {{--                        <div class="dropdown-menu" aria-labelledby="navbarDropdown1">--}}
                     {{--                            <a class="dropdown-item" href="/maps.php">Карта</a>--}}
                     {{--                            <a class="dropdown-item" href="/info.php">Информация</a>--}}
@@ -301,7 +342,7 @@
 
                     {{--                    <li class="nav-item dropdown my-auto">--}}
                     {{--                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown1" role="button"--}}
-                    {{--                           data-toggle="dropdown"--}}
+                    {{--                           data-bs-toggle="dropdown"--}}
                     {{--                           aria-haspopup="true" aria-expanded="false">Склад</a>--}}
                     {{--                        <div class="dropdown-menu" aria-labelledby="navbarDropdown1">--}}
                     {{--                            <a class="dropdown-item" href="/load.php?route=admin/sklad/sklad">Склады</a>--}}
@@ -345,6 +386,7 @@
 
             </div>
         </nav>
+    </div>
 
 
     {{--        <?php if (in_array($_SESSION['priority'], [0, 2, 4, 7])) { ?>--}}
