c732c48d4b217ebed67fe8600e88cbce0ab6b0a8
Vladimir Dadyka <dadyka.vova@gmail.com>
2022-04-14 14:15:49 +0300

    created order form, two selects installed pinia state manager

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,9 @@
+import { createPinia } from "pinia";
+
 require("./bootstrap");
 import { createApp } from "vue";
 import OrderForm from "./components/OrderForm";
 const app = createApp({});
 app.component("OrderForm", OrderForm);
+app.use(createPinia());
 app.mount("#app");
