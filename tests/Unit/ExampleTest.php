98d15e706771c9ae286f593568975fa55da2fa0b
2020 <fyodor.kazaryan@bk.ru>
2022-05-07 16:22:06 +0300

    made: test for get_installation_salary method

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,21 +2,27 @@
 
     namespace Tests\Unit;
 
-    use App\Models\User;
-    use Illuminate\Foundation\Testing\DatabaseMigrations;
-    use Illuminate\Foundation\Testing\RefreshDatabase;
-    use PHPUnit\Framework\TestCase;
+    use Tests\TestCase;
     use App\Services\Calculator\Interfaces\Calculator;
     use Tests\CreatesApplication;
 
     class ExampleTest extends TestCase
     {
-        use CreatesApplication, DatabaseMigrations, RefreshDatabase;
+        use CreatesApplication;
 
         public function setUpDefaultActions() {
             $this->createApplication();
-            \Artisan::call('migrate');
+
+            \Artisan::call('migrate:fresh');
             \Artisan::call('db:seed');
+
+            $requestData = [
+                'categories' => 5,
+                'tissues' => 1,
+                'profiles' => 1,
+            ];
+
+            request()->merge($requestData);
         }
 
         /**
@@ -28,17 +34,20 @@
         public function get_installation_salary() {
             $this->setUpDefaultActions();
 
-            $requestData = [
-                'categories' => 5,
-                'tissues' => 1,
-                'profiles' => 1,
-            ];
-
-            request()->merge($requestData);
-
             $calculator = app(Calculator::class);
             $salary = $calculator->getInstallationSalary(8, 1, 1);
 
             self::assertTrue($salary->salary == 1050);
+        }
+
+        /**
+         * @return void
+         * @test
+         */
+        public function salary_for_difficulty() {
+            $this->setUpDefaultActions();
+
+            $calculator = app(Calculator::class);
+            self::assertTrue($calculator->salaryForDifficulty(1000, 2, 2) == 500);
         }
     }
